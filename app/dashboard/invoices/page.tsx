16934f7ef24a6f36936654f919f441685c134400
programmerhasan0 <programmerhasan0@gmail.com>
2024-12-12 20:49:13 +0600

    Data Mutation methods done

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,19 +4,22 @@ import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import { JSX, Suspense } from "react";
+import { Suspense } from "react";
+
 import { fetchInvoicesPages } from "@/app/lib/data";
 
-const Page: React.FC = async (props: {
-    searchParams?: { query: string; page: string };
-}): Promise<JSX.Element> => {
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
     const searchParams = await props.searchParams;
-    const query: string = searchParams?.query || "";
-    const currentPage: number = Number(searchParams?.page) || 1;
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
 
-    const totalPages: number = await fetchInvoicesPages(query);
-
-return (
+    return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
                 <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
@@ -36,6 +39,4 @@ return (
             </div>
         </div>
     );
-};
-
-export default Page;
+}
